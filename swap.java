a3ea26b014cc5bf39c1c97da298cd895f25ddc59
Agnik04 <karanagnik@gmail.com>
2022-04-08 12:52:41 +0530

    swaping two numbers

---
+++ swap.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class swap {
+    public static void main(String args[])
+    {
+        int a , b , c ;
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter two numbers to swap");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.printf("Before swaping \n%d %d",a,b);
+
+        c = a ;
+        a = b ;
+        b = c ;
+
+        System.out.printf("After swaping \n%d %d",a,b);
+
+        sc.close();
+    }
+    
+}
