43ccbba1319ac4e517375b83979ab2b57a8b8de8
Agnik04 <karanagnik@gmail.com>
2022-05-02 22:19:00 +0530

    Basic codeing

---
+++ Basic/day1.java
@@ -0,0 +1,46 @@
+//import java.util.Scanner;
+
+// James Gosling - Sun Microsystem
+public class L1{
+    public static void main(String args[]){
+
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+        // double a = 2.9876;
+        // System.out.printf("This is a = %.2f\n",a);
+
+        // integer range = -2^32 to 2^31-1
+        // long a = 434235622434l;
+        // float b = 4.5646f;
+
+        // Single line comment
+        /**
+         * Multi
+         * Line
+         * Comment
+         */
+
+        //Double >> Float >> Long >> Int >> Short >> Char >> byte
+        // Double a1 = 10.2;
+        // float a2 = 10.3f;
+        // long sum = (a1 + a2);
+
+        // int a = 23;
+        // int b = 34;
+        // int c = 45;
+        // int d = 56;
+        // System.out.println(a + b + " " + c + d);
+
+        // double a = Math.pow(4,2);
+        // System.out.println(a);
+
+        // Scanner sc = new Scanner(System.in);
+        // int b = sc.nextInt();
+        // float c = sc.nextFloat();
+        // String s = sc.nextLine();
+        // String s1 = sc.next();
+        // char ch = sc.next().charAt(0);
+        // sc.close();
+    }
+}
